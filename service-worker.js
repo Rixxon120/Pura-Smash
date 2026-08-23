7e661db3c8bc3f5cd1c9fc0c1c1118ac2ffb5534
Rixxon120 <38661599+Rixxon120@users.noreply.github.com>
2026-08-23 02:52:51 -0600

    v100

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="pura-smash-shell-v9";
-const APP_SHELL=["/","/index.html","/manifest.webmanifest","/assets/app.9c7781d9b6.min.js","/assets/styles.eb314dab60.min.css","/assets/icons/icon-192.png","/assets/icons/icon-512.png","/assets/icons/icon-maskable-192.png","/assets/icons/icon-maskable-512.png","/assets/icons/apple-touch-icon.png"];
+const CACHE_NAME="pura-smash-shell-v10";
+const APP_SHELL=["/","/index.html","/manifest.webmanifest","/assets/app.58ab91e645.min.js","/assets/styles.eb314dab60.min.css","/assets/icons/icon-192.png","/assets/icons/icon-512.png","/assets/icons/icon-maskable-192.png","/assets/icons/icon-maskable-512.png","/assets/icons/apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
