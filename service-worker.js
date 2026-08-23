cded1b21ee05101fcd1a3ac50e8da35bafad1534
Rixxon120 <38661599+Rixxon120@users.noreply.github.com>
2026-08-23 02:37:22 -0600

    mantener sesion iniciada

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME="pura-smash-shell-v5";
-const APP_SHELL=["/","/index.html","/manifest.webmanifest","/assets/app.cb461e7742.min.js","/assets/styles.eb314dab60.min.css","/assets/icons/icon-192.png","/assets/icons/icon-512.png","/assets/icons/icon-maskable-192.png","/assets/icons/icon-maskable-512.png","/assets/icons/apple-touch-icon.png"];
+const CACHE_NAME="pura-smash-shell-v6";
+const APP_SHELL=["/","/index.html","/manifest.webmanifest","/assets/app.c75ea2078c.min.js","/assets/styles.eb314dab60.min.css","/assets/icons/icon-192.png","/assets/icons/icon-512.png","/assets/icons/icon-maskable-192.png","/assets/icons/icon-maskable-512.png","/assets/icons/apple-touch-icon.png"];
 
 self.addEventListener("install",event=>{
   event.waitUntil(
